68aa93e8176c254ead1bfbdbab4cb4a36056b6d6
user <andervol2015@gmail.com>
2025-01-31 11:21:00 +0300

    HW-3 работа над ошибками-2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //Задача 1
         //Объявите переменные типа int, byte, short, long, float, double
-        int oneInt;
-        oneInt = 1000000000;
-        byte tooByte;
-        tooByte = 125;
-        short threeShort;
-        threeShort = 32000;
-        long fourLong;
-        fourLong = 57885885;
-        float fiveFloat;
-        fiveFloat = 3.123456F;
-        double sixDouble;
-        sixDouble = 1.123456789101112131415F;
+        int oneInt = 1000000000;
+        byte tooByte = 125;
+        short threeShort = 32000;
+        long fourLong = 57885885;
+        float fiveFloat = 3.123456F;
+        double sixDouble = 1.123456789101112131415F;
         System.out.println("Значение переменной 'a' с типом 'int' равно " + oneInt);
         System.out.println("Значение переменной 'b' с типом 'byte' равно " + tooByte);
         System.out.println("Значение переменной 'с' с типом 'short' равно " + threeShort);
@@ -26,7 +20,7 @@ public class Main {
         double oD;
         oD = 27.12;
         long dV;
-        dV = 987678965549L;
+        dV = 987_678_965_549L;
         float tR;
         tR = 2.786F;
         short cHe;
@@ -44,20 +38,20 @@ public class Main {
         byte ekaterinaAndreevna = 30;
         short totalPaper = 480;
         int toEvereone = totalPaper / (lyudmilaPavlovna + annaSergeevna + ekaterinaAndreevna);
-        System.out.println("На каждого ученика рассчитано " + (toEvereone) + " листов бумаги");
+        System.out.println("На каждого ученика рассчитано " + toEvereone + " листов бумаги");
 
         //Задача 4
         byte bottle = 16;
         byte minutes = 2;
-        int bottle2 = bottle / minutes;
-        int za_20Min = bottle2 * 20;
-        int zaSutki = bottle2 * 60 * 24;
-        int za_3Day = bottle2 * zaSutki * 3;
-        int za_30Day = bottle2 * zaSutki * 30;
-        System.out.println("За 20 минут машина произвела " + za_20Min + " штук бутылок");
+        int bottlesPerMinute = bottle / minutes;
+        int za20Min = bottlesPerMinute * 20;
+        int zaSutki = za20Min * 3 * 24;
+        int za3Day = zaSutki * 30;
+        int za30Day = za3Day * 10;
+        System.out.println("За 20 минут машина произвела " + za20Min + " штук бутылок");
         System.out.println("За сутки машина произвела " + zaSutki + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + za_3Day + " штук бутылок");
-        System.out.println("За 1 месяц (30 дней) машина произвела " + za_30Day + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + za3Day + " штук бутылок");
+        System.out.println("За 1 месяц (30 дней) машина произвела " + za30Day + " штук бутылок");
 
         //Задача 5
         byte totalJar = 120;
@@ -81,11 +75,11 @@ public class Main {
         System.out.println("Общий вес в Килограммах " + kg);
 
         //Задача 7
-        short diet_1 = 250;
-        short diet_2 = 500;
+        short dietOne = 250;
+        short dietToo = 500;
         byte targetDiet = 7;
-        int gm250 = targetDiet * 1000 / diet_1;
-        int gm500 = targetDiet * 1000 / diet_2;
+        int gm250 = targetDiet * 1000 / dietOne;
+        int gm500 = targetDiet * 1000 / dietToo;
         int averageDay = (gm250 + gm500) / 2;
         System.out.println("При Диете-1 и сбросом веса по 250гр. в день, потребуется " + gm250 + " дней");
         System.out.println("При Диете-2 и сбросом веса по 500гр. в день, потребуется " + gm500 + " дней");
